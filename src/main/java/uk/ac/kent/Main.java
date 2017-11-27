192aac0286f0d34a08bd944766db007ff8594b05
nl253 <nl253@kent.ac.uk>
2017-11-27 19:47:31 +0000

    Initial commit
    - add LICENSE, README, NOTICE - create directory structure - pom.xml
    (project object model) for Apache Maven

---
+++ src/main/java/uk/ac/kent/Main.java
@@ -0,0 +1,31 @@
+package main.java.uk.ac.kent;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * The entry point into the application.
+ */
+
+@SuppressWarnings({"PublicMethodNotExposedInInterface", "ClassNamingConvention", "PublicConstructor", "ClassWithoutLogger"})
+public final class Main extends Application {
+
+    private static final double HEIGHT = 275.0;
+    private static final double WIDTH = 300.0;
+
+    @Override
+    public final void start(final Stage stage) throws java.io.IOException {
+        final Parent root = FXMLLoader
+                .load(getClass().getResource("sample.fxml"));
+        stage.setTitle("Hello World");
+        stage.setScene(new Scene(root, WIDTH, HEIGHT));
+        stage.show();
+    }
+
+    public static void main(final String[] args) {
+        launch(args);
+    }
+}
