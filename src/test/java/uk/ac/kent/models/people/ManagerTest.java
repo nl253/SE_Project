ea66baff6e648546231cde449c58750629487b26
nl253 <nl253@kent.ac.uk>
2017-12-13 14:15:52 +0000

    Add tests

---
+++ src/test/java/uk/ac/kent/models/people/ManagerTest.java
@@ -0,0 +1,15 @@
+package uk.ac.kent.models.people;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+}
