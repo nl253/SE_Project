bce486aa63f5fa31139876f8fff9e3a44c90c968
nl253 <nl253@kent.ac.uk>
2017-12-13 14:15:52 +0000

    Add tests

---
+++ src/test/java/uk/ac/kent/models/records/AnnualReviewRecordTest.java
@@ -0,0 +1,16 @@
+package uk.ac.kent.models.records;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnnualReviewRecordTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+}
---
+++ src/test/java/uk/ac/kent/models/records/EmploymentDetailsRecordTest.java
@@ -0,0 +1,15 @@
+package uk.ac.kent.models.records;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmploymentDetailsRecordTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+}
