11dcec888bb5f25a52b0e42c98dd991b3810516f
nl253 <nl253@kent.ac.uk>
2018-01-03 17:32:05 +0000

    Add tests

---
+++ src/test/java/uk/ac/kent/models/records/recommendations/TerminationBaseRecommendationTest.java
@@ -0,0 +1,13 @@
+package uk.ac.kent.models.records.recommendations;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class TerminationBaseRecommendationTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @AfterEach
+    void tearDown() {}
+}
